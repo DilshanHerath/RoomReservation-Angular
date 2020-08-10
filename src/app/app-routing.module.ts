96226874a6ab2dd6905e5e8107c4c8c9cc15b31f
DilshanHerth <dilshanmadushanka981@gmail.com>
2020-08-10 10:06:15 +0530

    Project updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RoomReserveComponent} from './component/room-reserve/room-reserve.component';
+import {HomeComponent} from './component/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {path: 'home', component: HomeComponent},
+  {path: 'reserveRoom', component: RoomReserveComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {AlertModule} from 'ngx-alerts';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import {NgxSpinnerModule} from 'ngx-spinner';
+import { RoomReserveComponent } from './component/room-reserve/room-reserve.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RoomReserveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
+    AlertModule.forRoot({maxMessages: 5, timeout: 5000, position: 'right'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
